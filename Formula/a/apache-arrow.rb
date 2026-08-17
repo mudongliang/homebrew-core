bec155255bfb409c9e094eaf884857cd7f6a18e1
Michael Cho <michael@michaelcho.dev>
2026-08-16 20:30:32 -0400

    apache-arrow: revision bump (llvm -> llvm@22)

--- Formula/a/apache-arrow.rb
+++ Formula/a/apache-arrow.rb
@@ -5,7 +5,7 @@ class ApacheArrow < Formula
   mirror "https://archive.apache.org/dist/arrow/arrow-25.0.1/apache-arrow-25.0.1.tar.gz"
   sha256 "43d5de0a581f43cf63a2c06b4dcf13b9ff6fcd800f023324596e5781093bc500"
   license "Apache-2.0"
-  revision 1
+  revision 2
   compatibility_version 3
   head "https://github.com/apache/arrow.git", branch: "main"
 
@@ -29,7 +29,7 @@ class ApacheArrow < Formula
   depends_on "aws-sdk-cpp"
   depends_on "brotli"
   depends_on "grpc"
-  depends_on "llvm"
+  depends_on "llvm@22"
   depends_on "lz4"
   depends_on "openssl@3"
   depends_on "protobuf"
@@ -66,7 +66,7 @@ class ApacheArrow < Formula
 
     args = %W[
       -DCMAKE_INSTALL_RPATH=#{rpath}
-      -DLLVM_ROOT=#{formula_opt_prefix("llvm")}
+      -DLLVM_ROOT=#{formula_opt_prefix("llvm@22")}
       -DARROW_DEPENDENCY_SOURCE=SYSTEM
       -DARROW_ACERO=ON
       -DARROW_COMPUTE=ON
